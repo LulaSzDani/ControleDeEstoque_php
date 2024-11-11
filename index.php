559d52ad6bbc73dd428e0cdd8e825d7b07d1f486
LulaSzDani <lulafelipe7@gmail.com>
2024-11-11 20:15:49 -0300

    oi

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css\estilo.css">
+</head>
+<body>
+	<header>
+		
+		<img src="imagens\img.png.png" alt="">
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Produtos</a></li>
+				<li><a href="#">Fornecedores</a></li>
+				<li><a href="#">Categorias</a></li>
+			</ul>
+        </nav>
+
+	</header>
+
+	<main class="container">
+		<h2><a href="#">Conteúdo</a></h2>
+	</main>
+	<footer>
+		<h2><a href="#">Rodapé</a></h2>
+	</footer>
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
